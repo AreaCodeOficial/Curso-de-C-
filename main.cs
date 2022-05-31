2e7b3c51f14152ed935b6212947cf5eaa4737d78
Area Code <96663387+AreaCodeOficial@users.noreply.github.com>
2022-05-31 17:08:47 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Aula04{
+  public static void Main (string[] args) {
+    int num = 0;
+    Console.WriteLine(num);
+  }
+  void teste(){
+    int num = 0;
+    Console.WriteLine(num);
+  }
+}
